a44364125908cc48840a79c73d97f02b847a99f6
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-03-25 17:27:09 +0000

    "components"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './componenets/Footer';
+import Header from './componenets/Header';
+import Main from './componenets/Main';
+import Nav from './componenets/Nav';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
